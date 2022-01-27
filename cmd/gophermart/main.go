0c4201fa3c9d5ec3ca6aef8b52c1ebe736925bee
Panov Alexey <alexeykapanov@gmail.com>
2022-01-27 08:44:00 +0300

    Use parameters from config in server running.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"github.com/PanovAlexey/accumulated_points_reward_system/config"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/handlers/http"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/servers"
+	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
+	config := config.NewConfig()
 	handler := http.NewHandler()
 	server := new(servers.Server)
 
-	if err := server.Run("8080", handler.InitRoutes()); err != nil {
+	if err := server.Run(config, handler.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
+	}
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("error loading env variables: %s", err.Error())
 	}
 }
