a7223d3af2ac684b1448ff99f76ca6c7bec0db8c
Panov Alexey <alexeykapanov@gmail.com>
2022-01-29 07:23:00 +0300

    Create interface for logger.

---
+++ internal/infrastructure/logging/loggerInterface.go
@@ -0,0 +1,12 @@
+package logging
+
+type LoggerInterface interface {
+	Debug(msg string, fields ...interface{})
+	Info(msg string, fields ...interface{})
+	Warn(msg string, fields ...interface{})
+	Error(msg string, fields ...interface{})
+	Panic(msg string, fields ...interface{})
+	Fatal(msg string, fields ...interface{})
+	Fatalf(format string, v ...interface{})
+	Close()
+}
