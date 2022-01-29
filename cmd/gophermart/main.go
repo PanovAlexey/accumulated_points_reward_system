cae980d69df4f24d7b251c694208022ce341d9ae
Panov Alexey <alexeykapanov@gmail.com>
2022-01-29 08:45:00 +0300

    Use custom logger.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,23 +3,27 @@ package main
 import (
 	"github.com/PanovAlexey/accumulated_points_reward_system/config"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/handlers/http"
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/infrastructure/logging"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/servers"
 	"github.com/joho/godotenv"
-	"log"
 )
 
 func main() {
+	logger := logging.GetLogger()
+	defer logger.Close()
+
+	if err := godotenv.Load(); err != nil {
+		logger.Fatalf("error loading env variables: %s", err.Error())
+	}
+
 	config := config.NewConfig()
 	handler := http.NewHandler()
 	server := new(servers.Server)
 
 	if err := server.Run(config, handler.InitRoutes()); err != nil {
-		log.Fatalf("error occurred while running http server: %s", err.Error())
+		logger.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
 
 func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Printf("error loading env variables: %s", err.Error())
-	}
 }
