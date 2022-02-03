b57c940cbaec381ded15ef24c53a8a370685ee59
Panov Alexey <alexeykapanov@gmail.com>
2022-02-03 07:58:00 +0300

    Create repositories and services in main function for DI.

--- internal/handlers/http/http.go
+++ internal/handlers/http/http.go
@@ -1,17 +1,33 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/application/service"
+	"github.com/gin-gonic/gin"
+)
 
-type httpHandler struct{}
+type httpHandler struct {
+	userRegistrationService *service.UserRegistration
+}
 
-func NewHandler() *httpHandler {
-	return &httpHandler{}
+func NewHandler(userRegistrationService *service.UserRegistration) *httpHandler {
+	return &httpHandler{
+		userRegistrationService: userRegistrationService,
+	}
 }
 
 func (h *httpHandler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	router.GET("/ping", h.HandlePing)
+
+	api := router.Group("/api")
+	{
+		user := api.Group("/user")
+		{
+			user.POST("/register", h.register)
+			user.POST("/login", h.login)
+		}
+	}
 
 	return router
 }
