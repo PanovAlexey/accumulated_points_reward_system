da72b92de1e70754b71136447403dc48a258cfe5
Panov Alexey <alexeykapanov@gmail.com>
2022-02-03 07:16:00 +0300

    Create an entity for User.

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS users
 (
     id            BIGSERIAL NOT NULL UNIQUE,
     username      VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    password      VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
