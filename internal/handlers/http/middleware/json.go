ac67cf76d33c2e606605d2146d84637a38c7a7ec
Panov Alexey <alexeykapanov@gmail.com>
2022-02-03 08:21:00 +0300

    Create JSON Content-Type middleware.

---
+++ internal/handlers/http/middleware/json.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func JSON() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Header.Get("Content-Type") != "application/json" {
+			http.Error(c.Writer, "Invalid Content-Type", http.StatusBadRequest)
+			return
+		}
+	}
+}
