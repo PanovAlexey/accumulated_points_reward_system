e587e27549d5dd9d4da910ac197afd658eb61c91
Panov Alexey <alexeykapanov@gmail.com>
2022-02-04 19:33:00 +0300

    Create postgres connector.

---
+++ internal/infrastructure/databases/postgresConnector.go
@@ -0,0 +1,22 @@
+package databases
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB(databaseDsn string) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", databaseDsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
