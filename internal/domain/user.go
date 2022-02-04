a4a877815c7e653b5d2f4d4e5f0f3744e3a18206
Panov Alexey <alexeykapanov@gmail.com>
2022-02-04 19:55:00 +0300

    Rename  field to .

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,6 +2,6 @@ package domain
 
 type User struct {
 	Id       int    `json:"-" db:"id"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Login    string         `json:"login" db:"login" binding:"required"`
+	Password string         `json:"password" binding:"required"`
 }
