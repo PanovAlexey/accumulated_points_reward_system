85f380ac238c0ba84e50056a01b4015e8b4abd6f
Panov Alexey <alexeykapanov@gmail.com>
2022-05-03 07:55:00 +0300

    Do not use pointer in user entity for id field.

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -3,7 +3,7 @@ package domain
 import "database/sql"
 
 type User struct {
-	Id       *sql.NullInt64 `json:"-" db:"id"`
-	Login    string         `json:"login" db:"login" binding:"required"`
-	Password string         `json:"password" binding:"required"`
+	Id       sql.NullInt64 `json:"-" db:"id"`
+	Login    string        `json:"login" db:"login" binding:"required"`
+	Password string        `json:"password" binding:"required"`
 }
