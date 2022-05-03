3f1440f7b21a0f9f9cca8f7ee620dd3bafa1ce25
Panov Alexey <alexeykapanov@gmail.com>
2022-05-03 09:35:00 +0300

    Implement user registration handler.

--- internal/handlers/http/registerHandler.go
+++ internal/handlers/http/registerHandler.go
@@ -1,11 +1,32 @@
 package http
 
 import (
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/domain"
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/handlers/http/responses"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (h *httpHandler) register(c *gin.Context) {
+	var user domain.User
 
-	c.String(http.StatusOK, "OK")
+	if err := c.BindJSON(&user); err != nil {
+		responses.NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		h.logger.Error(err.Error())
+		return
+	}
+
+	_, err := h.userRegistrationService.Register(user)
+
+	if err != nil {
+		responses.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	token, err := h.userRegistrationService.GenerateToken(user.Login, user.Password)
+
+	c.Header("Authorization", token)
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
