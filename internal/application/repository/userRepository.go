7cd805adc366fc1163803563c29f6d4599e2877c
Panov Alexey <alexeykapanov@gmail.com>
2022-05-06 06:53:00 +0300

    Create a separate section for entities.

--- internal/application/repository/userRepository.go
+++ internal/application/repository/userRepository.go
@@ -1,11 +1,11 @@
 package repository
 
 import (
-	"github.com/PanovAlexey/accumulated_points_reward_system/internal/domain"
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/domain/entity"
 )
 
 type UserRepository interface {
-	CreateUser(user domain.User) (domain.User, error)
-	GetUser(login, password string) (domain.User, error)
+	CreateUser(user entity.User) (entity.User, error)
+	GetUser(login, password string) (entity.User, error)
 	IsLoginExist(login string) (bool, error)
 }
