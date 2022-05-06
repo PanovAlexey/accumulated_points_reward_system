15d267dc0cf42b6df91b004bbc375232c441a6f9
Panov Alexey <alexeykapanov@gmail.com>
2022-05-06 07:35:00 +0300

    Fix user aurh dto. Rename username field in database.

--- internal/domain/dto/userAuthDto.go
+++ internal/domain/dto/userAuthDto.go
@@ -1,6 +1,6 @@
 package dto
 
 type UserAuthDto struct {
-	Login    string `json:"username" binding:"required"`
+	Login    string `json:"login" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
