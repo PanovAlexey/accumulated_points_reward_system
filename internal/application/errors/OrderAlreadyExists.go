01d6619059d76fcc4902598410c4957f6bffacea
Panov Alexey <alexeykapanov@gmail.com>
2022-05-16 11:11:00 +0300

    Create an error entity to try to recreate the order.

---
+++ internal/application/errors/OrderAlreadyExists.go
@@ -0,0 +1,5 @@
+package errors
+
+import "errors"
+
+var ErrorOrderAlreadyExists = errors.New("already exists")
---
+++ internal/application/errors/OrderAlreadySent.go
@@ -0,0 +1,5 @@
+package errors
+
+import "errors"
+
+var ErrorOrderAlreadySent = errors.New("already sent")
