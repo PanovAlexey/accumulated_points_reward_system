edaa2248a4e4da583cb26f66fb20ae90b4f59f97
Panov Alexey <alexeykapanov@gmail.com>
2022-05-18 09:17:00 +0300

    Create authorization middleware.

---
+++ internal/handlers/http/middleware/authorization.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+const (
+	authorizationHeader = "Authorization"
+	userCtx             = "userId"
+)
+
+type TokenParsing interface {
+	ParseToken(accessToken string) (int, error)
+}
+
+func Authorization(tokenParserService TokenParsing) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader(authorizationHeader)
+
+		if header == "" {
+			c.JSON(http.StatusUnauthorized, "empty auth header")
+			c.Abort()
+			return
+		}
+
+		headerParts := strings.Split(header, " ")
+
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, "invalid auth header")
+			c.Abort()
+			return
+		}
+
+		if len(headerParts[1]) == 0 {
+			c.JSON(http.StatusUnauthorized, "token is empty")
+			c.Abort()
+			return
+		}
+
+		userId, err := tokenParserService.ParseToken(headerParts[1])
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+
+		c.Set(userCtx, userId)
+	}
+}
