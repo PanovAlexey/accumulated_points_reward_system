817744e962c2277377ae47e8377337549fc41646
Panov Alexey <alexeykapanov@gmail.com>
2022-05-22 07:49:00 +0300

    Fix migration for creating order table.

--- internal/infrastructure/databases/migrations/up/3_create_orders_table.up.sql
+++ internal/infrastructure/databases/migrations/up/3_create_orders_table.up.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS orders
 (
     id        BIGSERIAL NOT NULL UNIQUE,
     user_id   BIGINT CONSTRAINT user_id_fk references users,
-    password  VARCHAR(255) NOT NULL,
-    number    INT NOT NULL,
+    number    BIGINT NOT NULL,
     status    INT CONSTRAINT status_id_fk references order_status,
     PRIMARY KEY(id)
 );
