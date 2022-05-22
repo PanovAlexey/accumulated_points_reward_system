bf0f5572e66adf78b9928eecd21a07f3a26da666
Panov Alexey <alexeykapanov@gmail.com>
2022-05-22 08:11:00 +0300

    Create constructors for order entity.

--- internal/domain/entity/order.go
+++ internal/domain/entity/order.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"database/sql"
+	"strconv"
 )
 
 type Order struct {
@@ -9,4 +10,22 @@ type Order struct {
 	Number string        `json:"number" db:"number" binding:"required"`
 	Status sql.NullInt64 `json:"status" db:"status" binding:"required"`
 	UserID sql.NullInt64 `json:"user_id" db:"user_id" binding:"required"`
+}
+
+func NewOrderByNumber(number string) Order {
+	return Order{
+		Number: number,
+	}
+}
+
+func NewOrder(number int64, status int, userID int64) Order {
+	var userIDNullInt, statusIDNullInt sql.NullInt64
+	userIDNullInt.Scan(userID)
+	statusIDNullInt.Scan(status)
+
+	return Order{
+		Number: strconv.FormatInt(number, 10),
+		Status: statusIDNullInt,
+		UserID: userIDNullInt,
+	}
 }
