39415858df22f92ffbfad083fcd5a388e1aff90a
Panov Alexey <alexeykapanov@gmail.com>
2022-05-23 06:11:00 +0300

    Create service for order statuses getting.

---
+++ internal/application/service/orderStatusGetter.go
@@ -0,0 +1,33 @@
+package service
+
+type orderStatusGetter struct {
+}
+
+func GetOrderStatusGetter() orderStatusGetter {
+	return orderStatusGetter{}
+}
+
+func (service orderStatusGetter) GetStatuses() map[int]string {
+	return map[int]string{
+		1: "REGISTERED",
+		2: "INVALID",
+		3: "PROCESSING",
+		4: "PROCESSED",
+	}
+}
+
+func (service orderStatusGetter) GetRegisteredStatusID() int {
+	return 1
+}
+
+func (service orderStatusGetter) GetInvalidStatusID() int {
+	return 2
+}
+
+func (service orderStatusGetter) GetProcessingStatusID() int {
+	return 3
+}
+
+func (service orderStatusGetter) GetProcessedStatusID() int {
+	return 4
+}
