806f57edbedf4f1b708c6f3123b65ae6a61781dc
Panov Alexey <alexeykapanov@gmail.com>
2022-05-28 07:23:00 +0300

    Mapping order entity to dto.

--- internal/handlers/http/getOrdersHandler.go
+++ internal/handlers/http/getOrdersHandler.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/domain/dto"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/handlers/http/responses"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func (h *httpHandler) getOrders(c *gin.Context) {
@@ -18,7 +20,33 @@ func (h *httpHandler) getOrders(c *gin.Context) {
 
 	orders, err := h.orderLoaderService.GetOrdersByUserID(int64(userCtxValue.(int)))
 
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"temp info": "endpoint in progress",
-	})
+	if err != nil {
+		responses.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		h.logger.Error(err.Error())
+
+		return
+	}
+
+	if orders == nil {
+		responses.NewErrorResponse(c, http.StatusNoContent, "no orders")
+		h.logger.Error("no orders", userCtxValue)
+
+		return
+	}
+
+	var orderOutputs []dto.OrderOutputDto
+
+	for _, order := range *orders {
+		orderOutputs = append(
+			orderOutputs,
+			dto.OrderOutputDto{
+				Number:     order.Number,
+				Status:     h.orderLoaderService.GetStatusNameByID(int(order.Status.Int64)),
+				Accrual:    500,
+				UploadedAt: time.Now().Format(time.RFC3339),
+			},
+		)
+	}
+
+	c.JSON(http.StatusOK, orderOutputs)
 }
