63ec7e9e28e43c182af90daf38ef493fda61d6ac
Panov Alexey <alexeykapanov@gmail.com>
2022-05-28 07:53:00 +0300

    Add migration for creating payments table.

---
+++ internal/infrastructure/databases/migrations/up/5_create_payments_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS payments
+(
+    id           BIGSERIAL NOT NULL UNIQUE,
+    user_id      BIGINT CONSTRAINT user_id_payment_fk references users,
+    order_id     BIGINT NOT NULL CONSTRAINT order_id_payment_fk references users,
+    processed_at VARCHAR NOT NULL,
+    sum          DECIMAL(12, 2),
+    PRIMARY KEY(id)
+);
