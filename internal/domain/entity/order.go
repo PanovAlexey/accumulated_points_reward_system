d5197c8b8329c775a8b7f1a3a92cefaa13b40573
Panov Alexey <alexeykapanov@gmail.com>
2022-05-28 06:55:00 +0300

    Add UploadedAt field to order entity.

--- internal/domain/entity/order.go
+++ internal/domain/entity/order.go
@@ -3,13 +3,15 @@ package entity
 import (
 	"database/sql"
 	"strconv"
+	"time"
 )
 
 type Order struct {
-	ID     sql.NullInt64 `json:"-" db:"id"`
-	Number string        `json:"number" db:"number" binding:"required"`
-	Status sql.NullInt64 `json:"status" db:"status" binding:"required"`
-	UserID sql.NullInt64 `json:"user_id" db:"user_id" binding:"required"`
+	ID         sql.NullInt64 `json:"-" db:"id"`
+	Number     string        `json:"number" db:"number" binding:"required"`
+	Status     sql.NullInt64 `json:"status" db:"status" binding:"required"`
+	UserID     sql.NullInt64 `json:"user_id" db:"user_id" binding:"required"`
+	UploadedAt string        `json:"uploaded_at" db:"uploaded_at" binding:"required"`
 }
 
 func NewOrder(number int64, status int, userID int64) Order {
@@ -18,8 +20,9 @@ func NewOrder(number int64, status int, userID int64) Order {
 	statusIDNullInt.Scan(status)
 
 	return Order{
-		Number: strconv.FormatInt(number, 10),
-		Status: statusIDNullInt,
-		UserID: userIDNullInt,
+		Number:     strconv.FormatInt(number, 10),
+		Status:     statusIDNullInt,
+		UserID:     userIDNullInt,
+		UploadedAt: time.Now().Format(time.RFC3339),
 	}
 }
