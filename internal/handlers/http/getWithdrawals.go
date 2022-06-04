c0034a51d0f698f06b7b1eca9b666cdc88ed0d5c
Panov Alexey <alexeykapanov@gmail.com>
2022-06-04 15:23:00 +0300

    Create endpoint to get all user withdrawals.

---
+++ internal/handlers/http/getWithdrawals.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"github.com/PanovAlexey/accumulated_points_reward_system/internal/handlers/http/responses"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *httpHandler) getWithdrawals(c *gin.Context) {
+	userCtxValue, isExist := c.Get(h.userRegistrationService.GetUserCtx())
+
+	if isExist == false {
+		responses.NewErrorResponse(c, http.StatusInternalServerError, "it is no info about user in context.")
+		h.logger.Error("it is no info about user in context.")
+
+		return
+	}
+
+	withdrawals, err := h.paymentManagement.GetWithdrawals(int64(userCtxValue.(int)))
+
+	if err != nil {
+		responses.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		h.logger.Error(err.Error())
+
+		return
+	}
+
+	if len(withdrawals) == 0 {
+		responses.NewErrorResponse(c, http.StatusNoContent, "No withdrawals")
+
+		return
+	}
+
+	c.JSON(http.StatusOK, withdrawals)
+}
