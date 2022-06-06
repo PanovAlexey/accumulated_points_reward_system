5a7ac895fa8403fddcbfd07995ad9f8f96cbcc7b
Panov Alexey <alexeykapanov@gmail.com>
2022-06-06 16:59:00 +0300

    Fix constraints in payments table.

--- internal/infrastructure/databases/migrations/up/5_create_payments_table.up.sql
+++ internal/infrastructure/databases/migrations/up/5_create_payments_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS payments
 (
     id           BIGSERIAL NOT NULL UNIQUE,
     user_id      BIGINT CONSTRAINT user_id_payment_fk references users,
-    order_id     BIGINT NOT NULL CONSTRAINT order_id_payment_fk references users,
+    order_id     BIGINT NOT NULL CONSTRAINT order_id_payment_fk references orders,
     processed_at VARCHAR NOT NULL,
     sum          DECIMAL(12, 2),
     PRIMARY KEY(id)
