35435e901ddc6f597594f18f3dd19f90da1c09e4
Panov Alexey <alexeykapanov@gmail.com>
2022-06-14 14:39:00 +0300

    Fix ToDo in code.

--- internal/infrastructure/repository/orderRepository.go
+++ internal/infrastructure/repository/orderRepository.go
@@ -6,6 +6,7 @@ import (
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/domain/entity"
 	"github.com/PanovAlexey/accumulated_points_reward_system/internal/infrastructure/databases"
 	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
 	"strconv"
 )
 
@@ -88,13 +89,37 @@ func (repository orderRepository) SetOrderStatusID(orderID int64, statusID int) 
 func (repository orderRepository) GetOrdersByStatusesID(statusesID []int) (*[]entity.Order, error) {
 	var orders []entity.Order
 
-	err := repository.db.Select(
-		&orders,
-		"SELECT * FROM "+databases.OrdersTableNameConst+" WHERE status = 1 OR status = 3", // @ToDo replace hardcode numbers
-	)
+	if len(statusesID) == 0 {
+		return &orders, nil
+	}
 
-	if err == sql.ErrNoRows {
-		return nil, nil
+	stmt, err := repository.db.Prepare("SELECT * FROM " + databases.OrdersTableNameConst + " WHERE status = ANY($1)")
+
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := stmt.Query(pq.Array(statusesID))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var o entity.Order
+		err = rows.Scan(&o.ID, &o.Status, &o.Number, &o.UserID, &o.UploadedAt)
+
+		if err != nil {
+			return &orders, err
+		}
+
+		orders = append(orders, o)
 	}
 
 	return &orders, nil
