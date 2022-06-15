abbf528d2a735ff46bfd522a469be5c5e4f5be47
Panov Alexey <alexeykapanov@gmail.com>
2022-06-15 09:51:07 +0300

    Send payment to the repository as a structure instead of a set of fields.

--- internal/application/repository/paymentRepository.go
+++ internal/application/repository/paymentRepository.go
@@ -7,7 +7,7 @@ import (
 
 type PaymentRepository interface {
 	GetBalance(userID int64) (float64, error)
-	Create(userID, orderID int64, sum float64) (entity.Payment, error)
+	Create(payment entity.Payment) (entity.Payment, error)
 	GetOrderIDToPaymentMap(orderIDList []int64) (map[int64]entity.Payment, error)
 	GetTotalWithdrawn(userID int64) (float64, error)
 	GetWithdrawnPayments(userID int64) ([]dto.WithdrawalsOutputDto, error)
