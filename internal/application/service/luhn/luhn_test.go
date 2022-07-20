a05c4f747fe7020585d88873bb0fd98d6933f418
Panov Alexey <alexeykapanov@gmail.com>
2022-07-20 13:33:00 +0300

    Fix luhn algorithm example.

--- internal/application/service/luhn/luhn_test.go
+++ internal/application/service/luhn/luhn_test.go
@@ -1,7 +1,7 @@
 package luhn
 
 import (
-	"log"
+	"fmt"
 )
 
 func Example() {
@@ -14,29 +14,25 @@ func Example() {
 
 	// process the correct order number
 	err := luhnChecker.Validate(correctOrderNumber)
-	log.Print("order number ")
-	log.Print(correctOrderNumber)
+	fmt.Print("Order number ")
+	fmt.Print(correctOrderNumber)
 
 	if err != nil {
-		log.Print(" is wrong. ")
+		fmt.Print(" is wrong. ")
 	} else {
-		log.Print(" is correct. ")
+		fmt.Print(" is correct. ")
 	}
-
-	// Output: ["order number 79927398713 is correct. "]
-
-	log.Println("")
 
 	// process the wrong order number
 	err = luhnChecker.Validate(wrongOrderNumber)
-	log.Print("order number ")
-	log.Print(wrongOrderNumber)
+	fmt.Print("Order number ")
+	fmt.Print(wrongOrderNumber)
 
 	if err != nil {
-		log.Println(" is wrong. ")
+		fmt.Print(" is wrong. ")
 	} else {
-		log.Println(" is correct. ")
+		fmt.Print(" is correct. ")
 	}
 
-	// Output: ["order number 111222333 is wrong. "]
+	// Output:Order number 79927398713 is correct. Order number 111222333 is wrong.
 }
