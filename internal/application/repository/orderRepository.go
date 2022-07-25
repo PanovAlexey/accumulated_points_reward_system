1c635a7d20b444f9f3a6094423ec84b204f9237d
Panov Alexey <alexeykapanov@gmail.com>
2022-07-25 14:33:00 +0300

    Create interface for deleting orders by user login.

--- internal/application/repository/orderRepository.go
+++ internal/application/repository/orderRepository.go
@@ -9,6 +9,7 @@ type OrderRepository interface {
 	CreateOrder(order entity.Order) (entity.Order, error)
 	GetOrder(number int64) (*entity.Order, error)
 	GetOrdersByUserID(userID int64) (*[]entity.Order, error)
+	DeleteOrdersByUserID(userID int64) error
 	SetOrderStatus(orderID int64, status string) error
 	GetOrdersByStatuses([]string) (*[]entity.Order, error)
 }
