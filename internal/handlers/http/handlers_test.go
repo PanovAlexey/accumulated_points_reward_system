5b3adb5cb672d617e78b0de3768761fa9ef7faa3
Panov Alexey <alexeykapanov@gmail.com>
2022-07-26 12:27:00 +0300

    Add temporary generated data for test.

--- internal/handlers/http/handlers_test.go
+++ internal/handlers/http/handlers_test.go
@@ -1,1 +1,7 @@
 package http
+
+var testData = map[string]string{
+	"user_login":      "test@test.test",
+	"user_password":   "password",
+	"user_auth_token": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTM5NjA2MTAsImlhdCI6MTY1MzkxNzQxMCwidXNlcl9pZCI6NH0.UusEPIYIilvGkSf68BTNxioHs0LajSXT24OwnB7VE5A",
+}
