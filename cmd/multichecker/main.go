fef386e06b0ece31826484263e13dc4eb94ee972
Panov Alexey <alexeykapanov@gmail.com>
2022-08-10 16:21:00 +0300

    Use staticcheck SA analyzers.

--- cmd/multichecker/main.go
+++ cmd/multichecker/main.go
@@ -30,6 +30,7 @@ import (
 	"golang.org/x/tools/go/analysis/passes/unreachable"
 	"golang.org/x/tools/go/analysis/passes/unsafeptr"
 	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"honnef.co/go/tools/staticcheck"
 )
 
 func main() {
@@ -66,5 +67,89 @@ func main() {
 		unusedresult.Analyzer,
 	)
 
+	analyzerList = append(analyzerList,
+		// staticcheck.io package SA class analyzers
+		staticcheck.Analyzers["SA1000"],
+		staticcheck.Analyzers["SA1001"],
+		staticcheck.Analyzers["SA1002"],
+		staticcheck.Analyzers["SA1003"],
+		staticcheck.Analyzers["SA1004"],
+		staticcheck.Analyzers["SA1005"],
+		staticcheck.Analyzers["SA1006"],
+		staticcheck.Analyzers["SA1007"],
+		staticcheck.Analyzers["SA1008"],
+		staticcheck.Analyzers["SA1010"],
+		staticcheck.Analyzers["SA1011"],
+		staticcheck.Analyzers["SA1012"],
+		staticcheck.Analyzers["SA1013"],
+		staticcheck.Analyzers["SA1014"],
+		staticcheck.Analyzers["SA1015"],
+		staticcheck.Analyzers["SA1016"],
+		staticcheck.Analyzers["SA1017"],
+		staticcheck.Analyzers["SA1018"],
+		staticcheck.Analyzers["SA1019"],
+		staticcheck.Analyzers["SA1020"],
+		staticcheck.Analyzers["SA1021"],
+		staticcheck.Analyzers["SA1023"],
+		staticcheck.Analyzers["SA1024"],
+		staticcheck.Analyzers["SA1025"],
+		staticcheck.Analyzers["SA1026"],
+		staticcheck.Analyzers["SA1027"],
+		staticcheck.Analyzers["SA1028"],
+		staticcheck.Analyzers["SA1029"],
+
+		staticcheck.Analyzers["SA2000"],
+		staticcheck.Analyzers["SA2001"],
+		staticcheck.Analyzers["SA2002"],
+		staticcheck.Analyzers["SA2003"],
+
+		staticcheck.Analyzers["SA3000"],
+		staticcheck.Analyzers["SA3001"],
+
+		staticcheck.Analyzers["SA4000"],
+		staticcheck.Analyzers["SA4001"],
+		staticcheck.Analyzers["SA4003"],
+		staticcheck.Analyzers["SA4004"],
+		staticcheck.Analyzers["SA4006"],
+		staticcheck.Analyzers["SA4008"],
+		staticcheck.Analyzers["SA4009"],
+		staticcheck.Analyzers["SA4010"],
+		staticcheck.Analyzers["SA4011"],
+		staticcheck.Analyzers["SA4012"],
+		staticcheck.Analyzers["SA4013"],
+		staticcheck.Analyzers["SA4014"],
+		staticcheck.Analyzers["SA4015"],
+		staticcheck.Analyzers["SA4016"],
+		staticcheck.Analyzers["SA4017"],
+		staticcheck.Analyzers["SA4018"],
+		staticcheck.Analyzers["SA4019"],
+		staticcheck.Analyzers["SA4020"],
+		staticcheck.Analyzers["SA4021"],
+
+		staticcheck.Analyzers["SA5000"],
+		staticcheck.Analyzers["SA5001"],
+		staticcheck.Analyzers["SA5002"],
+		staticcheck.Analyzers["SA5003"],
+		staticcheck.Analyzers["SA5004"],
+		staticcheck.Analyzers["SA5005"],
+		staticcheck.Analyzers["SA5007"],
+		staticcheck.Analyzers["SA5008"],
+		staticcheck.Analyzers["SA5009"],
+		staticcheck.Analyzers["SA5010"],
+		staticcheck.Analyzers["SA5011"],
+
+		staticcheck.Analyzers["SA6000"],
+		staticcheck.Analyzers["SA6001"],
+		staticcheck.Analyzers["SA6002"],
+		staticcheck.Analyzers["SA6003"],
+		staticcheck.Analyzers["SA6005"],
+
+		staticcheck.Analyzers["SA9001"],
+		staticcheck.Analyzers["SA9002"],
+		staticcheck.Analyzers["SA9003"],
+		staticcheck.Analyzers["SA9004"],
+		staticcheck.Analyzers["SA9005"],
+ 
+	)
 	multichecker.Main(analyzerList...)
 }
